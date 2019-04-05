79387bd7f827e5549febcb32f55415c12816f834
Jacob Stolberg <jacobstolberg@Jacobs-Air.lan>
2019-04-04 22:20:01 -0500

    Some Structure for Javascript

---
+++ js/app.js
@@ -0,0 +1,67 @@
+class Player{
+	fish1 = null;
+	fish2 = null;
+	fish3 = null;
+	fish4 = null;
+	fish5 = null;
+	total = null:
+
+	castRod(){
+		// makes rod shake as well as 
+	}
+	catchFish(){
+
+	}	
+}
+
+const game = {
+	time: 0,
+	timeInt: null,
+	payLake(){
+		//resets game to play again
+	}
+	runGame(){
+		// runs all necesary functions/ jquery to make game work 
+	}
+	fishOClock(){
+		// timer for game!
+	}
+	
+}
+
+
+$('').on('click', () =>{
+
+});
+
+$('').on('click', () =>{
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
