03bc4cbcd493c302e9877362128c349fefe44cc7
Jacob Stolberg <jacobstolberg@Jacobs-MacBook-Air.local>
2019-04-05 14:18:03 -0500

    page structured

--- js/app.js
+++ js/app.js
@@ -4,14 +4,26 @@ class Player{
 	fish3 = null;
 	fish4 = null;
 	fish5 = null;
-	total = null:
+	total = null;
 
 	castRod(){
-		// makes rod shake as well as 
-	}
+		// makes rod shake as well as initiates the catchFish function  
+	};
 	catchFish(){
-
+		//alerts the player they have a Fish On!
+		//starts the meter to reel in the fish aka run the animation function for div id=block
+		//if caught puts fish on display hook and adds weigght to total catch value! 
 	}	
+}
+
+class Fish {
+	constructor(size,weight){
+		this.size = size;
+		this.weight = weight;
+	}
+	breakTheLine(){
+		// if the player does really bad on the bar challenge then the fish will break the line and swim away!
+	}
 }
 
 const game = {
@@ -19,16 +31,18 @@ const game = {
 	timeInt: null,
 	payLake(){
 		//resets game to play again
-	}
+	},
 	runGame(){
 		// runs all necesary functions/ jquery to make game work 
-	}
+	},
 	fishOClock(){
-		// timer for game!
+		this.timeInt = setInterval(() => {
+			console.log(this.time);
+			this.time += 1
+				
+		},2000)
 	}
-	
 }
-
 
 $('').on('click', () =>{
 
